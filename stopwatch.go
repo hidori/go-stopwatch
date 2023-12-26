f22b308afa9ecc0d611944acc74705829a3928d0
Hiroaki SHIBUKI <hidori@users.noreply.github.com>
2023-12-26 12:51:45 +0900

    Added impl(s)

---
+++ stopwatch.go
@@ -0,0 +1,39 @@
+package stopwatch
+
+import "time"
+
+type Stopwatch struct {
+	start *time.Time
+	end   *time.Time
+}
+
+func NewStopwatch() *Stopwatch {
+	return &Stopwatch{}
+}
+
+func (sw *Stopwatch) Start() {
+	t := time.Now()
+	sw.start = &t
+}
+
+func (sw *Stopwatch) Stop() {
+	t := time.Now()
+	sw.end = &t
+}
+
+func (sw *Stopwatch) Reset() {
+	sw.start = nil
+	sw.end = nil
+}
+
+func (sw *Stopwatch) Duration() time.Duration {
+	if sw.start != nil {
+		if sw.end == nil {
+			return time.Since(*sw.start)
+		}
+
+		return sw.end.Sub(*sw.start)
+	}
+
+	return 0
+}
